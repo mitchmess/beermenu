91b66b89cfe5bda91eccfc299cddf8861f5b8def
Mitch Messecar <mitch@rocksolidrestaurants.com>
2022-01-27 11:30:24 -0800

    main2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import Retool from 'react-retool';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+    <Retool url="https://hopsndrops.retool.com/embedded/public/628dc73d-f421-4773-b6ee-0b921fa0780c"/>
+  );
 }
 
 export default App;
